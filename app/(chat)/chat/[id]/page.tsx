0ccc9576ebfcbb9ead384f22b5a7b55b6387b205
Muhammad Saad <155970679+MuhammadSaad897423@users.noreply.github.com>
2025-05-02 22:08:06 +0500

    Update page.tsx
    updated

--- app/(chat)/chat/[id]/page.tsx
+++ app/(chat)/chat/[id]/page.tsx
@@ -13,7 +13,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
   const { id } = params;
   const chat = await getChatById({ id });
-
+import type { Attachment, UIMessage } from 'ai';
   if (!chat) {
     notFound();
   }
